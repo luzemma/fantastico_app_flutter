54032b86b1021395e264fb13044602d0f16cca82
Luzemma Gonzalez <luzemmagonzalez@MacBook-Pro-de-Luzemma.local>
2023-10-14 16:12:01 -0600

    feat: adds stack in product item

--- lib/ui/product/product_item.dart
+++ lib/ui/product/product_item.dart
@@ -20,54 +20,44 @@ class ProductItem extends StatelessWidget {
           ),
         ],
       ),
-      child: Column(
+      child: Stack(
         children: [
-          Row(
+          Column(
             children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                height: 150,
-                child: Image.network(
-                  'https://im.fantasticocomic.com/im/b/5d/0e00422bb9453398496a77bd6cabe.jpg',
-                  width: 80,
-                  height: 150,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Crime Syndicate #3 Cvr B Babs Tarr Card Stock Var',
-                      style: textTheme.bodyMedium?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      'DC Comics',
-                      style: textTheme.bodyMedium?.copyWith(
-                        color: ColorHelper.lightGray,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
+              Row(
                 children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.favorite,
-                      color: Colors.red,
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    height: 150,
+                    child: Image.network(
+                      'https://im.fantasticocomic.com/im/b/5d/0e00422bb9453398496a77bd6cabe.jpg',
+                      width: 80,
+                      height: 150,
+                      fit: BoxFit.contain,
                     ),
                   ),
-                  const SizedBox(
-                    height: 16,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Crime Syndicate #3 Cvr B Babs Tarr Card Stock Var',
+                          style: textTheme.bodyMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'DC Comics',
+                          style: textTheme.bodyMedium?.copyWith(
+                            color: ColorHelper.lightGray,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 4, right: 8),
@@ -78,36 +68,44 @@ class ProductItem extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    height: 48,
-                  ),
                 ],
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                child: ElevatedButton.icon(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(ColorHelper.primaryBlue),
+                  ),
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Agregar al carrito',
+                    style: textTheme.bodyMedium?.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
               ),
             ],
           ),
-          Container(
-            alignment: Alignment.centerRight,
-            margin: const EdgeInsets.symmetric(horizontal: 8),
-            child: ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(ColorHelper.primaryBlue),
-              ),
+          Positioned(
+            top: 5,
+            right: 15,
+            child: IconButton(
               onPressed: () {},
               icon: const Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
-              ),
-              label: Text(
-                'Agregar al carrito',
-                style: textTheme.bodyMedium?.copyWith(
-                  color: Colors.white,
-                ),
+                Icons.favorite,
+                color: Colors.red,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 8,
           ),
         ],
       ),
