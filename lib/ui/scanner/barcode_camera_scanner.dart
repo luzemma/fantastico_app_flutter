d4f5364f0386fc69ea7fcfe1f3372261973ee024
Luzemma Gonzalez <luzemmagonzalez@MacBook-Pro-de-Luzemma.local>
2023-10-20 16:47:18 -0600

    feat: adds barcode scanner with camera

---
+++ lib/ui/scanner/barcode_camera_scanner.dart
@@ -0,0 +1,165 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+
+class BarcodeCameraScanner extends StatefulWidget {
+  const BarcodeCameraScanner({
+    required this.onSelectImage,
+    super.key,
+  });
+
+  final Function(InputImage inputImage) onSelectImage;
+
+  @override
+  State<BarcodeCameraScanner> createState() => _BarcodeCameraScannerState();
+}
+
+class _BarcodeCameraScannerState extends State<BarcodeCameraScanner> {
+  static List<CameraDescription> _cameras = [];
+  CameraController? _cameraController;
+  int _cameraIndex = -1;
+
+  static const _orientations = {
+    DeviceOrientation.portraitUp: 0,
+    DeviceOrientation.landscapeLeft: 90,
+    DeviceOrientation.portraitDown: 180,
+    DeviceOrientation.landscapeRight: 270,
+  };
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initializeCameras();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _cameras.isEmpty ||
+            _cameraController == null ||
+            _cameraController?.value.isInitialized == false
+        ? const SizedBox.shrink()
+        : Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.black,
+              child: CameraPreview(
+                _cameraController!,
+              ),
+            ),
+          );
+  }
+
+  @override
+  void dispose() {
+    _stopLiveFeed();
+    super.dispose();
+  }
+
+  Future<void> _initializeCameras() async {
+    if (_cameras.isEmpty) {
+      _cameras = await availableCameras();
+    }
+    for (var i = 0; i < _cameras.length; i++) {
+      if (_cameras[i].lensDirection == CameraLensDirection.back) {
+        _cameraIndex = i;
+        break;
+      }
+    }
+    if (_cameraIndex != -1) {
+      await _startLiveFeed();
+    }
+  }
+
+  Future<void> _startLiveFeed() async {
+    final camera = _cameras[_cameraIndex];
+    _cameraController = CameraController(
+      camera,
+      // Set to ResolutionPreset.high. Do NOT set it to ResolutionPreset.max because for some phones does NOT work.
+      ResolutionPreset.high,
+      enableAudio: false,
+      imageFormatGroup: Platform.isAndroid
+          ? ImageFormatGroup.nv21
+          : ImageFormatGroup.bgra8888,
+    );
+    await _cameraController?.initialize();
+
+    await _cameraController?.startImageStream(_processCameraImage);
+
+    setState(() {});
+  }
+
+  Future _stopLiveFeed() async {
+    await _cameraController?.stopImageStream();
+    await _cameraController?.dispose();
+    _cameraController = null;
+  }
+
+  void _processCameraImage(CameraImage image) {
+    final inputImage = _inputImageFromCameraImage(image);
+    if (inputImage == null) return;
+    widget.onSelectImage(inputImage);
+  }
+
+  InputImage? _inputImageFromCameraImage(CameraImage image) {
+    if (_cameraController == null) return null;
+
+    // get image rotation
+    // it is used in android to convert the InputImage from Dart to Java: https://github.com/flutter-ml/google_ml_kit_flutter/blob/master/packages/google_mlkit_commons/android/src/main/java/com/google_mlkit_commons/InputImageConverter.java
+    // `rotation` is not used in iOS to convert the InputImage from Dart to Obj-C: https://github.com/flutter-ml/google_ml_kit_flutter/blob/master/packages/google_mlkit_commons/ios/Classes/MLKVisionImage%2BFlutterPlugin.m
+    // in both platforms `rotation` and `camera.lensDirection` can be used to compensate `x` and `y` coordinates on a canvas: https://github.com/flutter-ml/google_ml_kit_flutter/blob/master/packages/example/lib/vision_detector_views/painters/coordinates_translator.dart
+    final camera = _cameras[_cameraIndex];
+    final sensorOrientation = camera.sensorOrientation;
+    // print(
+    //     'lensDirection: ${camera.lensDirection}, sensorOrientation: $sensorOrientation, ${_controller?.value.deviceOrientation} ${_controller?.value.lockedCaptureOrientation} ${_controller?.value.isCaptureOrientationLocked}');
+    InputImageRotation? rotation;
+    if (Platform.isIOS) {
+      rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+    } else if (Platform.isAndroid) {
+      var rotationCompensation =
+          _orientations[_cameraController!.value.deviceOrientation];
+      if (rotationCompensation == null) return null;
+      if (camera.lensDirection == CameraLensDirection.front) {
+        // front-facing
+        rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+      } else {
+        // back-facing
+        rotationCompensation =
+            (sensorOrientation - rotationCompensation + 360) % 360;
+      }
+      rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
+      // print('rotationCompensation: $rotationCompensation');
+    }
+    if (rotation == null) return null;
+    // print('final rotation: $rotation');
+
+    // get image format
+    final format = InputImageFormatValue.fromRawValue(image.format.raw as int);
+    // validate format depending on platform
+    // only supported formats:
+    // * nv21 for Android
+    // * bgra8888 for iOS
+    if (format == null ||
+        (Platform.isAndroid && format != InputImageFormat.nv21) ||
+        (Platform.isIOS && format != InputImageFormat.bgra8888)) return null;
+
+    // since format is constraint to nv21 or bgra8888, both only have one plane
+    if (image.planes.length != 1) return null;
+    final plane = image.planes.first;
+
+    // compose InputImage using bytes
+    return InputImage.fromBytes(
+      bytes: plane.bytes,
+      metadata: InputImageMetadata(
+        size: Size(image.width.toDouble(), image.height.toDouble()),
+        rotation: rotation, // used only in Android
+        format: format, // used only in iOS
+        bytesPerRow: plane.bytesPerRow, // used only in iOS
+      ),
+    );
+  }
+}
--- lib/ui/scanner/scanner_screen.dart
+++ lib/ui/scanner/scanner_screen.dart
@@ -1,12 +1,92 @@
+import 'package:fantastico_app/ui/scanner/barcode_camera_scanner.dart';
+import 'package:fantastico_app/utils/color_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 
-class ScannerScreen extends StatelessWidget {
+class ScannerScreen extends StatefulWidget {
   const ScannerScreen({super.key});
 
   @override
+  State<ScannerScreen> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  BarcodeScanner? _barcodeScanner;
+  bool _canProcess = true;
+  bool _isBusy = false;
+
+  bool _scannerStarted = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (_scannerStarted) ...[
+                BarcodeCameraScanner(
+                  onSelectImage: _scannerProcessImage,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+              ],
+              ElevatedButton.icon(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(ColorHelper.primaryBlue),
+                ),
+                onPressed: _onStartScanner,
+                icon: const Icon(
+                  Icons.qr_code_scanner_outlined,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  '${_scannerStarted ? 'Apagar' : 'Encender'} el escáner',
+                  style: textTheme.bodyMedium?.copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  void _onStartScanner() {
+    _barcodeScanner ??= BarcodeScanner(formats: [BarcodeFormat.qrCode]);
+    setState(() {
+      _scannerStarted = !_scannerStarted;
+    });
+  }
+
+  Future<void> _scannerProcessImage(InputImage inputImage) async {
+    if (!_scannerStarted || !_canProcess || _isBusy) return;
+
+    if (_barcodeScanner == null) return;
+
+    _isBusy = true;
+    final barcodes = await _barcodeScanner!.processImage(inputImage);
+    if (barcodes.isNotEmpty) {
+      Future.delayed(const Duration(milliseconds: 500), () {
+        Navigator.of(context).pop(barcodes[0]);
+      });
+    } else {
+      _isBusy = false;
+    }
+  }
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    _barcodeScanner?.close();
+    super.dispose();
   }
 }
